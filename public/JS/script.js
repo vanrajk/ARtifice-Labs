c91f2af7f4d5eed5da30f2ae0a3554872e665efc
Patel Mark <patelmark5330@gmail.com>
2024-08-06 18:10:44 +0530

    Js update

--- public/JS/script.js
+++ public/JS/script.js
@@ -45,3 +45,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+
+function toggleSidebar() {
+  var sidebar = document.getElementById("sidebar");
+  if (sidebar.style.width === "95vw") {
+      sidebar.style.width = "0";
+  } else {
+      sidebar.style.width = "95vw";
+  }
+}
